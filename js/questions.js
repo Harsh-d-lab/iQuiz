59b3b9a985ee05bfc1a2537e9d194be4e9e634d6
Harsh Dhama <dhamaharsh9@gmail.com>
2022-11-11 11:59:25 +0530

    upload

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does JS stand for?",
+    answer: "Javascript",
+    options: [
+      "Javascript",
+      "Junoscript",
+      "Jasonscript",
+      "jeescript"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Application Program Interface",
+      "Application Programming Interface",
+      "Applied Programming Interface",
+      "Application Programming Internet"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does CDN stand for?",
+    answer: " content delivery network",
+    options: [
+      " content delivery networking",
+      " contempt delivery network",
+      " content delivery network",
+      " content deliverable network"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does DOM stand for?",
+    answer: "Document object model",
+    options: [
+      "Document object modelling",
+      "Document objectified model",
+      "Documentation object model",
+      "Document object model"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does GUI stand for?",
+    answer: "Graphical user interface",
+    options: [
+      "Graphical user interface",
+      "Graph user interface",
+      "Graphical users interfacing",
+      "Graphical useless interface",
+    ]
+  },
+];
