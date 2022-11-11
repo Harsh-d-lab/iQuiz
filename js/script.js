2fb768143c35a34334ed63d5ca18ced56f2ea434
Harsh Dhama <dhamaharsh9@gmail.com>
2022-11-11 13:03:59 +0530

    js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,6 @@ let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
 let counter;
-let widthValue = 0;
 const restart_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
 restart_quiz.onclick = ()=>{
@@ -113,40 +112,31 @@ function colourCounter() {
     document.getElementById("reds").innerHTML = notanswered;
     document.getElementById("whites").innerHTML =
       10 - answered - flagged - notanswered;
-}
+  }
 let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
-  let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
-  function optionSelected(answer){
-      clearInterval(counter);
-      clearInterval(counterLine);
-      colourCounter();
-      let userAns = answer.textContent;
-      let correcAns = questions[que_count].answer;
-      const allOptions = option_list.children.length;
-      if(userAns == correcAns){ 
-          userScore += 1;
-          answer.classList.add("correct");
-          answer.insertAdjacentHTML("beforeend", tickIconTag);
-          console.log("Correct Answer");
-          console.log("Your correct answers = " + userScore);
-      }else{
-          answer.classList.add("incorrect");
-          answer.insertAdjacentHTML("beforeend", crossIconTag);
-          console.log("Wrong Answer");
-  
-          for(i=0; i < allOptions; i++){
-              if(option_list.children[i].textContent == correcAns){
-                  option_list.children[i].setAttribute("class", "option correct");
-                  option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
-                  console.log("Auto selected correct answer.");
-              }
-          }
-      }
-      for(i=0; i < allOptions; i++){
-          option_list.children[i].classList.add("disabled");
-      }
-      next_btn.classList.add("show");
-      prev_btn.classList.add("show");
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+function optionSelected(answer){
+    clearInterval(counter);
+    colourCounter();
+    let userAns = answer.textContent;
+    let correcAns = questions[que_count].answer;
+    const allOptions = option_list.children.length;
+    if(userAns == correcAns){ 
+        userScore += 1;
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend", tickIconTag);
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIconTag);
+        console.log("Wrong Answer");
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.classList.add("show");
+    prev_btn.classList.add("show");
 }
 function showResult(){
     info_box.classList.remove("activeInfo");
@@ -193,8 +183,6 @@ function startTimer(time){
             }
             prev_btn.classList.add("show");
             next_btn.classList.add("show");
-            clear_btn.classList.add("show");
-            flag_btn.classList.add("show");
         }
     }
 }
